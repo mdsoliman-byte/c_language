f2ba2f7c8f994d84929c39babb3eb06c4c481ad7
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-10 21:02:14 +0600

    bynary number systme range 0 to 1

--- notes.c
+++ notes.c
@@ -1,3 +1,13 @@
 // decimale number systme : decimale number systme is a humen redable number , its also call as (base 10) number systme
 // 0 1 2 3 4 5 6 7 8 9
 // decimale numbur systme range : 0 to 9
+
+// Binary number systme : bynary number systme is a machine number systme , its also called as base 2 number systme 
+//  binary number systme range : 0 to 1 
+// 2    2   3   1
+// 2    2   2   2
+// 1    1   0   0
+// 2*2*1 + 2*2*1 + 2*3*0 + 2*1*0
+// 4 + 4 + 0 + 0 
+// 8 + 0
+// 8 
