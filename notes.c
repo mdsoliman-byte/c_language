34f3411fa5f15985ef9f0cb5ae3fbfbc967997d9
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-10 20:51:41 +0600

    decimale number systme range 0 to 9

---
+++ notes.c
@@ -0,0 +1,3 @@
+// decimale number systme : decimale number systme is a humen redable number , its also call as (base 10) number systme
+// 0 1 2 3 4 5 6 7 8 9
+// decimale numbur systme range : 0 to 9
