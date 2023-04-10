d9bd08de90276e2ce557bfe35fe11af81d5fc816
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-10 20:43:48 +0600

    variable capter is done somthing

--- variable.c
+++ variable.c
@@ -16,20 +16,25 @@ int main()
     // motherAge= 30;
     // printf("Your corunt age %d", myage);
     // printf("My Age Is %d %d %d", myage, fatheraGae, motherAge);
-    int numberA, numberB, num_Rc, sum;
-    numberA = 10;
-    numberB = 20;
-    num_Rc = 12;
-    sum = numberA + numberB - num_Rc;
-    printf(" your number %d", sum);
+    // int numberA, numberB, num_Rc, sum;
+    // numberA = 10;
+    // numberB = 20;
+    // num_Rc = 12;
+    // sum = numberA + numberB - num_Rc;
+    // printf(" your number %d", sum);
+    /* ****************************** */
+    int number1 = 10, number2 = 20, number3 = 40, number5 = 80;
+    int result = (number1 + number2) * number5 / number3;
+    printf("hello im shakil from bangladesh , im a web developer , and im learning c lenguage first time ,( %d + %d) * %d / %d" , number1, number2, number5 , number3);
+    printf("your program result is : %d", result);
 
     return 0;
 }
-
+// %d means decimal 
 // variable naming convantion
 // variable is a case sensetive
 //  int a valur = 10 (its not true , its given error,   we cannto use space or any spacial carector in variable name  )
 // int avalue = 10 (ite true , we can use underscor )
 
-// we cannot use any reserved keywords 
-// Example : if, els, switch , for , while , int , float , long , double 
+// we cannot use any reserved keywords
+// Example : if, els, switch , for , while , int , float , long , double
