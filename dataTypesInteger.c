12f342acdbaa1b19dd9ddf2fb378475e831d65e9
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-10 22:38:53 +0600

    data format int = %d , char = %c , float = %f , double = %lf

---
+++ dataTypesInteger.c
@@ -0,0 +1,27 @@
+// Fundamental data Type c
+// int , float , double , cher
+// char  = 1 byte = 8 bit
+// int  = 2 byte = 16 bit
+// float  = 4 byte = 32 bit
+// double  = 8 byte = 64 bit
+
+
+
+// formate
+// int = %d
+// char = %c
+// float = %f
+// double = %lf
+
+
+
+#include <stdio.h>
+int main()
+{
+    int a = 56;
+
+    // double b = 5.6;
+    double b = (double)a; // type cousting
+    // printf("A = %d\n", a);
+    printf("B = %lf", b);
+}
--- notes.c
+++ notes.c
@@ -2,12 +2,20 @@
 // 0 1 2 3 4 5 6 7 8 9
 // decimale numbur systme range : 0 to 9
 
-// Binary number systme : bynary number systme is a machine number systme , its also called as base 2 number systme 
-//  binary number systme range : 0 to 1 
+// Binary number systme : bynary number systme is a machine number systme , its also called as base 2 number systme
+//  binary number systme range : 0 to 1
 // 2    2   3   1
 // 2    2   2   2
 // 1    1   0   0
 // 2*2*1 + 2*2*1 + 2*3*0 + 2*1*0
-// 4 + 4 + 0 + 0 
+// 4 + 4 + 0 + 0
 // 8 + 0
-// 8 
+// 8
+//  note : 1 bytes = 8 bits 
+// range of 4 bit data
+// 4 bit data minimum value  = 0  (0    0   0   0)
+// 4 bit data maximum value = 15  (1    1   1   1) (fORMULA :  2^n - 1)
+
+// RANGE OF 4 BIT DATA (0   0   0   0) TO (1    1   1   1)
+
+//
