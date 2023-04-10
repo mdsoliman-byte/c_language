8073e1803bff0ebd4ab5c9fcc35ac0e55122e863
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-10 23:21:30 +0600

    formate

--- dataTypesInteger.c
+++ dataTypesInteger.c
@@ -4,24 +4,32 @@
 // int  = 2 byte = 16 bit
 // float  = 4 byte = 32 bit
 // double  = 8 byte = 64 bit
-
-
-
+//
 // formate
 // int = %d
 // char = %c
 // float = %f
 // double = %lf
-
-
-
+//
 #include <stdio.h>
 int main()
 {
-    int a = 56;
+    // int a = 56;
 
     // double b = 5.6;
-    double b = (double)a; // type cousting
+    // double b = (double)a; // type cousting
     // printf("A = %d\n", a);
-    printf("B = %lf", b);
+    // printf("B = %lf", &b); // & FOR VARIABLE ADDRESS
+    int a, b, sum;
+    scanf("%d ", &a);
+    scanf("%d ", &b);
+    // sum = a + b;
+
+    // printf("This Program rezult %d\n", sum);
+
+    //  int a, b, sum;
+    // scanf("%d", &a);
+    // scanf("%d", &b);
+    sum = a + b;
+    printf("Result = %d\n ", sum);
 }
