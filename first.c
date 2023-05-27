0f34124a6a5d1515043aada217e4e2f1130d6c54
mdsoliamn-io <developer.mdsoliman@gmail.com>
2023-05-27 09:53:52 -0700

    documments write

--- first.c
+++ first.c
@@ -1,6 +1,13 @@
+/*
+File name : first.c
+Author Name : MD Soliman 
+Date : 27/05/2023
+Description : My first c programming code 
+*/
 #include <stdio.h>
 int main()
 {
+    // text pourpus
     printf("Hello im soliman From C programming");
     return 0;
 }
